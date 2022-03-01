d947a89202bf5bd1f736255ff03245a92b37de2e
BilliAlpha <billi.pamege.300@gmail.com>
2022-03-01 13:26:22 +0100

    Remove modules from core plugin

--- src/main/java/fr/aquilon/minecraft/aquilonthings/modules/AQLVox/model/HTMLRouteHandler.java
+++
@@ -1,9 +0,0 @@
-package fr.aquilon.minecraft.aquilonthings.modules.AQLVox.model;
-
-/**
- * Created by Billi on 17/06/2018.
- *
- * @author Billi
- */
-@FunctionalInterface
-public interface HTMLRouteHandler extends GenericRouteHandler<HTMLResponse> {}
--- src/main/java/fr/aquilon/minecraft/aquilonthings/modules/AQLVox/model/RawRouteHandler.java
+++
@@ -1,9 +0,0 @@
-package fr.aquilon.minecraft.aquilonthings.modules.AQLVox.model;
-
-import fi.iki.elonen.NanoHTTPD;
-
-/**
- * @author Billi
- */
-@FunctionalInterface
-public interface RawRouteHandler extends GenericRouteHandler<NanoHTTPD.Response> {}
